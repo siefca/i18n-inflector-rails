807a61dea750e19bd52fb350d34609e259557bbd
Paweł Wilk <siefca@gnu.org>
2011-01-14 18:05:40 +0100

    Version 0.1.0

--- lib/i18n-inflector-rails/errors.rb
+++ lib/i18n-inflector-rails/errors.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 #
 # Author::    Paweł Wilk (mailto:pw@gnu.org)
-# Copyright:: (c) 2010 by Paweł Wilk
+# Copyright:: (c) 2011 by Paweł Wilk
 # License::   This program is licensed under the terms of {file:LGPL-LICENSE GNU Lesser General Public License} or {file:COPYING Ruby License}.
 # 
 # This file contains error reporting classes for I18n::Backend::Inflector module.
