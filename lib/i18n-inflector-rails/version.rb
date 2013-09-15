5a00ebf493b84da0bbac113c4c0b30a7893e799e
Paweł Wilk <siefca@gnu.org>
2013-09-15 12:18:46 +0200

    Release 1.0.7

--- lib/i18n-inflector-rails/version.rb
+++ lib/i18n-inflector-rails/version.rb
@@ -15,7 +15,7 @@ module I18n
       # @private
       EMAIL       = 'pw@gnu.org'
       # @private
-      VERSION     = '1.0.6'
+      VERSION     = '1.0.7'
       # @private
       NAME        = 'i18n-inflector-rails'
       # @private
